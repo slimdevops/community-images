1ccd5bb67f48fcd407d8f3bbb92dc1df2592870f
redocnib <kiran.txt@gmail.com>
2023-03-15 15:48:41 +0530

    feat: Gradle standalone image

---
+++ community-images/docker.io/library/gradle/latest/pre-setup.sh
@@ -0,0 +1,9 @@
+sudo apt-get -y update
+java -version
+#sudo apt-get -y install default-jre
+#sudo apt-get -y install default-jdk
+#java -version
+sudo apt-get purge -y --auto-remove openjdk*
+sudo apt-get purge -y --auto-remove temurin*
+sudo apt-get install -y openjdk-19-jdk
+java -version
