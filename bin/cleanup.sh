9e5c036f5a6b65f1356d18afff6da20ad33d93ed
redocnib <kiran.txt@gmail.com>
2023-03-17 17:04:59 +0530

    bug: caddy container shows unexpected halts, removing for now

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -5,7 +5,7 @@ if [[ $STAGE == "INSTRUMENT" ]]; then
     echo "Instrumented Step -- Gracetime set to 300 Seconds"
     docker stop -t 300 $PROJECT_CONTAINER_NAME
 else
-    docker stop -t 120 $PROJECT_CONTAINER_NAME
+    docker stop $PROJECT_CONTAINER_NAME
 fi
 
 docker wait $PROJECT_CONTAINER_NAME
@@ -14,7 +14,7 @@ docker rm --force $PROJECT_CONTAINER_NAME
 pid=$(sudo lsof -t -i:$APP_PORT)
 if [[ ! -z "$pid" ]]; then
     echo "Killing Process ($pid) Port: $APP_PORT"
-    sudo kill -9 $pid && true
+    sudo kill -9 $pid
 fi
 
 $HOOK_EXEC after-cleanup $STAGE
@@ -22,7 +22,7 @@ $HOOK_EXEC after-cleanup $STAGE
 if [[ "$PROJECT_CLEAN_DOCKER" == "true" ]]; then
     docker system df
     echo "Cleaning Docker"
-    docker system prune -af && true
+    docker system prune -af
     docker system df
 fi
 
