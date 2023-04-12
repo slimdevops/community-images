ea5b0bc02b91ca616a8ee227e391225e09ed9c91
redocnib <kiran.txt@gmail.com>
2023-04-12 18:12:24 +0530

    Failed Reporting

---
+++ bin/submit_status.sh
@@ -0,0 +1,27 @@
+url=$DISCORD_ENDPOINT
+#Start with RED
+color="15548997"
+status=$1
+content='' #"content": "<@slimdevl>",
+
+[[ "$status" == "success" ]] && {
+  color="45973" #Set to Green
+  content=""
+}
+
+
+generate_post_data() {
+  cat <<EOF
+{
+  $content
+  "embeds": [{
+    "title": "$REGISTRY/$NAMESPACE/$REPOSITORY:$TAG",
+    "description": "Status: $status\nBranch: $REPO_BRANCH\n\nRunner: $ACTION_RUNNER_URL",
+    "color": $color
+  }]
+}
+EOF
+}
+
+# POST request to The URL
+curl -H "Content-Type: application/json" -X POST -d "$(generate_post_data)" $url
