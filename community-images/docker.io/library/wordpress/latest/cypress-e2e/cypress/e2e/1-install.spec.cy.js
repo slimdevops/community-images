9a568b75bdeb5d46e03a2c1631ef4c1ba15aedc2
Kiran <kiran.txt@gmail.com>
2023-04-19 14:15:08 +0530

    Update 1-install.spec.cy.js

--- community-images/docker.io/library/wordpress/latest/cypress-e2e/cypress/e2e/1-install.spec.cy.js
+++ community-images/docker.io/library/wordpress/latest/cypress-e2e/cypress/e2e/1-install.spec.cy.js
@@ -5,14 +5,16 @@ describe('template spec', () => {
     /* ==== End Cypress Studio ==== */
     /* ==== Generated with Cypress Studio ==== */
     cy.get('#language-continue').click();
+    cy.get('#weblog_title').clear('D');
     cy.get('#weblog_title').type('Demo');
+    cy.get('#user_login').clear('d');
     cy.get('#user_login').type('demo');
     cy.get('.user-pass1-wrap').click();
     cy.get('#pass1').clear();
     cy.get('#pass1').type('demo@123');
     cy.get('.pw-checkbox').check();
-    cy.get('#admin_email').clear();
-    cy.get('#admin_email').type('user@email.com');
+    cy.get('#admin_email').clear('sreejiths@focaloid.com');
+    cy.get('#admin_email').type('sreejiths@focaloid.com');
     cy.get('#blog_public').check();
     cy.get('#submit').click();
     cy.get('.button').click();
