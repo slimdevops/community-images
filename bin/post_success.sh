a4f12cb5361799f1f2de708da4041e8ca31abdd6
Sreejith <sreejiths@focaloid.com>
2023-09-07 15:13:11 +0530

    Testing slimmed and fat names

---
+++ bin/post_success.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Set the URL you want to post data to
+URL="$TRIGGER_URL"
+
+# Define the data you want to send in JSON format
+DATA='{"registry": "'"$PROJECT_FULL_IMAGE_FAT"'", "namespace": "'"$PROJECT_FULL_IMAGE_SLIMMED"'", "repository": "'"$REPOSITORY"'", "digest": "'"$DIGEST"'", "tag": "'"$TAG"'"}'
+
+echo "$DATA"
+
+# Set the content type header
+CONTENT_TYPE="application/json"
+
+# Make the POST request using curl
+# curl -X POST -H "Content-Type: $CONTENT_TYPE" -d "$DATA" "$URL"
+
