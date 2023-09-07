a12aaba4a1a656dbaf59550c6110344f951f55b3
Sreejith <sreejiths@focaloid.com>
2023-09-07 12:45:01 +0530

    Added api to trigger lambda,Testing with a test

--- bin/add_to_cietl.sh
+++ bin/add_to_cietl.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo "Triggering CIETL"
+# Set the URL you want to post data to
+URL= $TRIGGER_URL
+
+# Define the data you want to send in JSON format
+DATA='{"registry": "'"$REGISTRY"'", "namespace": "'"$NAMESPACE"'", "repository": "'"$REPOSITORY"'", "digest": "'"$DIGEST"'", "tag": "'"$TAG"'"}'
+
+# Set the content type header
+CONTENT_TYPE="application/json"
+
+# Make the POST request using curl
+curl -X POST -H "Content-Type: $CONTENT_TYPE" -d "$DATA" "$URL"
+
