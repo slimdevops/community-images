8180ed521427f229e1a42b77a404b830fcb7c02f
Ivan Velichko <iximiuz@gmail.com>
2024-02-12 22:29:45 +0000

    fix package reachability CI step

--- bin/reachability_report.sh
+++ bin/reachability_report.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [[ "${PROJECT_SOURCE}" == "dockerfile" ]]; then
-  IMAGE=${PROJECT_TARGET_IMAGE}
-else
-  IMAGE=${PROJECT_ORIGINAL_IMAGE}
-fi
+IMAGE=${PROJECT_TARGET_IMAGE}
 
-slim reachability packages -q \
-  --connector ${CONNECTOR_ID} \
+echo "Computing package reachability for image ${IMAGE} using connector ${TARGET_CONNECTOR_ID}"
+
+slim reachability packages \
+  --connector ${TARGET_CONNECTOR_ID} \
   --ichp-nx ${SLIMAI_WORKFLOW_ID} \
   --print-all \
   --format json \
-  ${IMAGE} | tee reachable-packages.json
+  ${IMAGE} | tee ${PROJECT_FULL_PATH}/reachable-packages.json
+
